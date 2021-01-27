ac01cb5c1cb398faf18e2b4509c1cf441b85cdd3
Ashraf Otagun <Otagunolaniyi7@gmail.com>
2021-01-26 23:31:03 -0800

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,54 @@ void main() => runApp(MaterialApp(
                 fit: BoxFit.cover,
               ),
             ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ClipRect(
+                  child: SizedBox(
+                    height: 250.0,
+                    width: 410.0,
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+                      child: Container(
+                        alignment: Alignment.center,
+                        color: Colors.grey.withOpacity(0.1),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              margin: EdgeInsets.fromLTRB(5.0, 30.0, 5.0, 10.0),
+                              child: Text(
+                                'Want to build X apps?',
+                                style: TextStyle(
+                                  fontSize: 35.0,
+                                  color: Colors.white,
+                                  fontFamily: 'OpenSans',
+                                  letterSpacing: 2.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.all(7.0),
+                              child: Text(
+                                'Kickstart your career as a Flutter developer in just 3 months',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 17.0,
+                                  letterSpacing: 1.0,
+                                  fontFamily: 'OpenSans',
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
